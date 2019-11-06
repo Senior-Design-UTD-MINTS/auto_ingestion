052c6fc176b9671fbdb2ed444445de087186a47b
Beepbop0 <hey.nikhil42@gmail.com>
2019-11-05 18:56:08 -0600

    Update DB with multiple sensor data (#3)
    * make editting the upload spec only run once
    
    * test adding new sensor

--- ingestion.py
+++ ingestion.py
@@ -19,7 +19,7 @@ def get_druid_install():
 
 # have to split the URL like this because of pythons HTTP API
 MINTS_BASE_URL = "mintsdata.utdallas.edu:4200"
-MINTS_RESOURCE_URL = "/api/001e06305a12/latestData.json"
+MINTS_RESOURCES = {"001e06305a12", "001e06323a06"}
 DRUID_INSTALL = get_druid_install()
 DRUID_UPLOAD_SCRIPT = "bin/post-index-task"
 DRUID_URL = "http://localhost"
@@ -41,31 +41,54 @@ def setup_upload_spec():
     update_json_fh.close()
 
 
+
+def update_table_name(table_name):
+    # updating the update-spec.json to be located in our present directory
+    update_json_fh = open(DRUID_JSON_SPEC, "r")
+    update_json = json.loads(update_json_fh.read())
+    update_json_fh.close()
+
+    update_json["spec"]["dataSchema"]["dataSource"] = table_name
+    update_json_fh = open(DRUID_JSON_SPEC, "w")
+    update_json_fh.write(json.dumps(update_json))
+    update_json_fh.close()
+
+
 def job():
     print("ran at time {}".format(datetime.datetime.now()))
+    downloaded_entries = dict()
     # download latest data from MINTS
     mints_conn = http.client.HTTPConnection(MINTS_BASE_URL)
-    mints_conn.request("GET", MINTS_RESOURCE_URL)
-    raw_response_body = mints_conn.getresponse().read().decode("utf-8")
+    # initialize an emtpy list for each sensor
+    for sensor in MINTS_RESOURCES:
+        downloaded_entries[sensor] = list()
+
+    for sensor in MINTS_RESOURCES:
+        mints_conn.request("GET", "/api/{}/latestData.json/".format(sensor))
+        raw_response_body = mints_conn.getresponse().read().decode("utf-8")
+        entries_json = json.loads(raw_response_body)
+        for entry in entries_json["entries"]:
+            downloaded_entries[sensor].append(entry)
     mints_conn.close()
-    entries_json = json.loads(raw_response_body)
-
     # create a new json file to upload to druid
-    update_file = open(OUTPUT_JSON_FILENAME, "w")
-    for entry in entries_json["entries"]:
-        string = json.dumps(entry)
-        update_file.write(string)
-        update_file.write("\n")
 
-    update_file.close()
-
-    # run the script provided by druid to append to our datasource, as specified by the spec
-    script = "{}/{} --file {}/{} --url {}:{}".format(
-        DRUID_INSTALL, DRUID_UPLOAD_SCRIPT, BASE_DIR, DRUID_JSON_SPEC, DRUID_URL, DRUID_PORT)
-    print("attempting to execute the following script:")
-    print(script)
-    print("\n")
-    os.system(script)
+    for sensor_id, entries in downloaded_entries.items():
+        # write entries into updates.json file
+        update_file = open(OUTPUT_JSON_FILENAME, "w")
+        for entry in entries:
+            string = json.dumps(entry)
+            update_file.write(string)
+            update_file.write("\n")
+        update_file.close()
+        # change the table name of our updateSpec.json
+        update_table_name("MINTS_" + sensor_id)
+        # run the script provided by druid to append to our datasource, as specified by the spec
+        script = "{}/{} --file {}/{} --url {}:{}".format(
+            DRUID_INSTALL, DRUID_UPLOAD_SCRIPT, BASE_DIR, DRUID_JSON_SPEC, DRUID_URL, DRUID_PORT)
+        print("attempting to execute the following script:")
+        print(script)
+        print("\n")
+        os.system(script)
 
 
 # start of script
